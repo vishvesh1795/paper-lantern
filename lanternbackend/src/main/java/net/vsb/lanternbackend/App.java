dabbcfae6512b561280d54bd251b67ed6ab0c534
vishvesh <vishvesh208@gmail.com>
2017-09-22 15:33:16 +0530

    project successful

---
+++ lanternbackend/src/main/java/net/vsb/lanternbackend/App.java
@@ -0,0 +1,13 @@
+package net.vsb.lanternbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
